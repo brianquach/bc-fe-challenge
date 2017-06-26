32fc171e3a0679b74ade4f6dca4071d342d1d1ea
Brian Quach <13rianquach@gmail.com>
2017-06-25 17:24:29 -0700

    Feat: get company data api call

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,1 +1,64 @@
-console.log('init');
+var companyAPI = (function() {
+  'use strict';
+
+  var apiBaseURI;
+
+  function init() {
+    apiBaseURI = 'http://localhost:3000/';
+  }
+
+  function getCompanyInformation(options) {
+    options = options || {};
+    var query = options.q || '';
+    var start = options.start || 0;
+    var limit = options.limit || 10;
+    var laborTypes = options.laborTypes || [];
+
+    var queryString = '?q=' + query + '&start=' + start + '&limit=' + limit
+      + '&laborTypes=' + laborTypes.join(',');
+    var url = apiBaseURI + 'api/companies' + queryString;
+
+    tools.ajaxCall({
+      verb: 'GET',
+      url: url,
+      successCallBack: function(response) {
+        console.log(response);
+      },
+      failCallBack: function() {
+        console.log('error');
+      }
+    });
+  }
+
+  return {
+    init: init,
+    getCompanyInformation: getCompanyInformation
+  };
+})();
+
+var companySearch = (function() {
+  'use strict';
+
+  var $searchBox;
+  var $searchResults;
+
+  function init() {
+    $searchBox = document.getElementById('searchBox');
+    $searchResults = document.getElementById('searchResults');
+
+    $searchBox.addEventListener('keyup', function(event) {
+      companyAPI.getCompanyInformation();
+    });
+  }
+
+  function render() {
+    // TODO: render list of company name
+  }
+
+  return {
+    init: init
+  };
+})();
+
+companyAPI.init();
+companySearch.init();
